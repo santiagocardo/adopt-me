4faa7afb3b34b997f022da8712ffe0ed49182899
Santiago Cardona <scardona@lean-tech.io>
2020-05-22 20:26:52 -0500

    ⚡ Components with hooks. Custom hook and async API request

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import React from "react"
+import { render } from "react-dom"
+import SearchParams from "./SearchParams.js"
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-  ]);
-};
+  return (
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
+  )
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"))

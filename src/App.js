11034b16af0bce91506270c2f6c726debf6f3136
Santiago Cardona <scardona@lean-tech.io>
2020-05-29 18:14:43 -0500

    ✂️  Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react"
+import React, { useState, lazy, Suspense } from "react"
 import { render } from "react-dom"
 import { Router } from '@reach/router'
 
-import SearchParams from "./SearchParams"
-import Details from "./Details"
 import ThemeContext from "./ThemeContext"
 import NavBar from "./NavBar"
+
+const Details = lazy(() => import("./Details"))
+const SearchParams = lazy(() => import("./SearchParams"))
 
 const App = () => {
   const themeHook = useState('tomato')
@@ -15,10 +16,12 @@ const App = () => {
       <ThemeContext.Provider value={themeHook}>
         <div>
           <NavBar />
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <Suspense fallback={<h1>loadding route...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
